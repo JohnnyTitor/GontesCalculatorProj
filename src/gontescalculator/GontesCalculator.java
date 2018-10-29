de9c66da53a70268097fcc614f79f2169b712320
JohnnyTitor <38272097+JohnnyTitor@users.noreply.github.com>
2018-10-29 16:53:52 -0700

    Main class

---
+++ src/gontescalculator/GontesCalculator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gontescalculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Francisco
+ */
+public class GontesCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static Scanner keyboard = new Scanner(System.in);
+    public static void main(String[] args) {
+        keyboard.nextLine();
+        System.exit(0);
+    }
+    
+}
