6905467fe9c97b41b5e459742c477f85c4b64b69
JohnnyTitor <38272097+JohnnyTitor@users.noreply.github.com>
2018-10-29 20:09:24 -0700

    Driver class for the latest calc

--- src/gontescalculator/Calculator.java
+++ src/gontescalculator/Calculator.java
@@ -9,18 +9,6 @@ import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 
 
 public class Calculator {
- 
-    public enum TokenType
-    {
-        None,
-        Number,
-        Plus,
-        Minus,
-        Multiply,
-        Divide,
-        LeftP,
-        RightP
-    }
     
 
     public static String[] formatInput(String input)
@@ -32,9 +20,7 @@ public class Calculator {
         
         output = output.replaceAll("(?<number>\\d+(\\.\\d+)?)", " ${number} ");
         
-        output = output.replaceAll(" (?<ops>[+*/^()])", " ${ops} ");
-        
-        //output = output.replaceAll("(?<number>\\-+\\d+)?)", "~${number} ");
+        output = output.replaceAll("(?<ops>[+*/^()])", " ${ops} ");        
         
         // Removes all multi white spaces and replaces with a single space
         output = output.replaceAll(" {2,}", " ");
@@ -55,10 +41,10 @@ public class Calculator {
  
     public static Queue<String> convertInfixToRPN(String[] infixNotation) {
         Map<String, Integer> prededence = new HashMap<>();
-        prededence.put("/", 5);
-        prededence.put("*", 5);
-        prededence.put("+", 4);
-        prededence.put("-", 4);
+        prededence.put("/", 2);
+        prededence.put("*", 2);
+        prededence.put("+", 1);
+        prededence.put("-", 1);
         prededence.put("(", 0);
  
         Queue<String> Q = new LinkedList<>();
@@ -99,5 +85,61 @@ public class Calculator {
         }
  
         return Q;
+    }
+    
+    public static double solve(Queue<String> input)
+    {
+        Stack<Double> sol = new Stack<>();
+        double var1 = 0.0;
+        double var2 = 0.0;
+        
+        
+        
+        while(!input.isEmpty())
+        {
+            String token = input.remove();
+            
+            if(isNumber(token))
+            {
+                sol.push(Double.valueOf(token));
+            }
+            else
+            {
+                switch (token)
+                {
+                    case "+":
+                        if(sol.size() >=2)
+                        {
+                           var2 = sol.pop();
+                           var1 = sol.pop();
+                           sol.push(var1 + var2);
+                        }
+                        else
+                        {
+                            //throw new Exception("Error");
+                        }
+                        break;
+                    case "-":
+                        if(sol.size() >=2)
+                        {
+                           var2 = sol.pop();
+                           var1 = sol.pop();
+                           sol.push(var1 - var2);
+                        }
+                        else
+                        {
+                            //throw new Exception("Error");
+                        }
+                }
+            }
+            
+        }
+        
+        if(sol.size() == 1)
+        {
+            return sol.pop();
+        }
+        
+        return -1;
     }
 }
