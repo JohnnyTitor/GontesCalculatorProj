6cab99d64aa9a93db59e7debaf2d584a2b654389
JohnnyTitor <38272097+JohnnyTitor@users.noreply.github.com>
2018-10-29 18:32:23 -0700

    newest version, includes some testing

--- src/gontescalculator/Calculator.java
+++ src/gontescalculator/Calculator.java
@@ -1,10 +1,103 @@
-
 package gontescalculator;
 
-/**
- *
- * @author Francisco
- */
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+
 public class Calculator {
+ 
+    public enum TokenType
+    {
+        None,
+        Number,
+        Plus,
+        Minus,
+        Multiply,
+        Divide,
+        LeftP,
+        RightP
+    }
     
+
+    public static String[] formatInput(String input)
+    {
+        String output = input.toLowerCase();
+        
+        //removes all letters from the input
+        output = output.replaceAll("[a-z]", "");
+        
+        output = output.replaceAll("(?<number>\\d+(\\.\\d+)?)", " ${number} ");
+        
+        output = output.replaceAll(" (?<ops>[+*/^()])", " ${ops} ");
+        
+        //output = output.replaceAll("(?<number>\\-+\\d+)?)", "~${number} ");
+        
+        // Removes all multi white spaces and replaces with a single space
+        output = output.replaceAll(" {2,}", " ");
+        
+        output = output.replaceAll("\\s+", " ").trim();
+        
+        return output.split(" ");
+    }
+    
+    static boolean isNumber(String str) {
+        try{
+            Double.valueOf(str);
+            return true;
+        } catch(Exception e){
+            return false;
+        }
+    }
+ 
+    public static Queue<String> convertInfixToRPN(String[] infixNotation) {
+        Map<String, Integer> prededence = new HashMap<>();
+        prededence.put("/", 5);
+        prededence.put("*", 5);
+        prededence.put("+", 4);
+        prededence.put("-", 4);
+        prededence.put("(", 0);
+ 
+        Queue<String> Q = new LinkedList<>();
+        Stack<String> S = new Stack<>();
+ 
+        for (String token : infixNotation) {
+            if ("(".equals(token)) {
+                S.push(token);
+                continue;
+            }
+ 
+            if (")".equals(token)) {
+                while (!"(".equals(S.peek())) {
+                    Q.add(S.pop());
+                }
+                S.pop();
+                continue;
+            }
+            // an operator
+            if (prededence.containsKey(token)) {
+                while (!S.empty() && prededence.get(token) <= prededence.get(S.peek())) {
+                    Q.add(S.pop());
+                }
+                S.push(token);
+                continue;
+            }
+ 
+            if (isNumber(token)) {
+                Q.add(token);
+                continue;
+            }
+ 
+            throw new IllegalArgumentException("Invalid input");
+        }
+        // at the end, pop all the elements in S to Q
+        while (!S.isEmpty()) {
+            Q.add(S.pop());
+        }
+ 
+        return Q;
+    }
 }
