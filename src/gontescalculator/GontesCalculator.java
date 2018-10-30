e55c5b622f8e977d6b3209e2607971eb7541fb7f
JohnnyTitor <38272097+JohnnyTitor@users.noreply.github.com>
2018-10-29 18:32:38 -0700

    newest version, includes some testing

--- src/gontescalculator/GontesCalculator.java
+++ src/gontescalculator/GontesCalculator.java
@@ -5,20 +5,29 @@
  */
 package gontescalculator;
 
+import java.util.Arrays;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
  *
  * @author Francisco
  */
-public class GontesCalculator {
+public class GontesCalculator 
+{
 
     /**
      * @param args the command line arguments
      */
     
     public static Scanner keyboard = new Scanner(System.in);
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        String test = (keyboard.nextLine());
+        String[] formatted = (Calculator.formatInput(test));
+        System.out.println(Arrays.toString(formatted));
+        Queue q = Calculator.convertInfixToRPN(formatted);
+        System.out.println(q.toString());
         keyboard.nextLine();
         System.exit(0);
     }
